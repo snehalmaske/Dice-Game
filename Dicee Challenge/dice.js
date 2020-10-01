76725d407cf0e1e92ef50fb4c00798c98590dd3b
Snehal Maske <maskesnehal77@gmail.com>
2020-10-01 15:43:32 -0700

    Add files via upload

---
+++ Dicee Challenge/dice.js
@@ -0,0 +1,16 @@
+
+var randomNumber1 = Math.floor(((Math.random()*6)+1));
+var randomImages1 = "images/dice"+ randomNumber1 +".png";
+ document.querySelectorAll("img")[0].setAttribute("src", randomImages1);
+
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+var randomImages2 = "images/dice"+ randomNumber2 + ".png";
+ document.querySelectorAll("img")[1].setAttribute("src", randomImages2);
+
+ if(randomNumber1 > randomNumber2){
+   document.querySelector("h1").innerHTML = "Player 1 Wins ✌️";
+ } else if(randomNumber2 > randomNumber1 ){
+    document.querySelector("h1").innerHTML = "Player 2 Wins ✌️";
+ }else {
+   document.querySelector("h1").innerHTML = "It's a Draw! 🤟";
+ }
